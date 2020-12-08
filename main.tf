564e2ebd3daccbd85a8ba7edcc996b7c46e110b8
Benoît OYEZ <benoit.oyez@wescale.fr>
2020-12-08 18:30:59 +0000

    Some dummy code for a dice roll

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "random_integer" "dice_roll" {
+  min = 1
+  max = 6
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "result" {
+  value = "You rolled a ${random_integer.dice_roll.result}. Did you get lucky?"
+}
